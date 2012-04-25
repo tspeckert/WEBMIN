ad0a5a12ad4ee11e2607af4482fc08d206bdae8b
Thomas Speckert <tspeckert@gmail.com>
2012-04-25 13:02:36 +0200

    added 2 pages and the sql db dump

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head><title>Group 6 Recommendation System</title></head>
+<body>
+<div>Hello there.</div>
+<div>
+ <form action="like.php" method="get">
+  <p>To begin, please enter your e-mail address:</p>
+  <p><input type="text" name="email" /></p>
+  <p><input type="submit" value="Continue" /></p>
+ </form>
+</div>
+<?php
+
+?>
+</body>
+</html>
---
+++ like.php
@@ -0,0 +1,11 @@
+<html>
+<head><title>Group 6 Recommendation System</title></head>
+<body>
+<div>
+ Hello <?php echo htmlspecialchars($_GET["email"]); ?>, please select the restaurants you like below:
+</div>
+<div>
+<?php phpinfo(); ?>
+</div>
+</body>
+</html>
